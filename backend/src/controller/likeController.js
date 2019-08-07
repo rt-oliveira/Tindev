f5780567ac9181cebff6b64c454169f471dfe535
Rafael Teixeira <fael.teixeira@gmail.com>
2019-08-07 17:59:00 -0300

    Adicionada funcionalidade de dar like.

---
+++ backend/src/controller/likeController.js
@@ -0,0 +1,27 @@
+const Dev=require("../model/dev");
+
+module.exports={
+    async armazenar(req,res){
+
+        const {IdDev}=req.params;
+        const {usuario}=req.headers;
+
+        const usuarioLogado=await Dev.findById(usuario);
+        const usuarioASeDarLike=await Dev.findById(IdDev);
+
+        if(!usuarioASeDarLike){
+            return res.status(400).json({erro: "Desenvolvedor não existe"});
+        }
+
+        if(usuarioASeDarLike.likes.includes(usuarioLogado._id)){
+            console.log("Deu match!");
+        }
+
+        usuarioLogado.likes.push(usuarioASeDarLike._id);
+        // Adiciona o usuário que deu like em sua array de likes.
+        
+        await usuarioLogado.save();
+
+        return res.json(usuarioLogado);
+    }
+}
--- backend/src/rotas.js
+++ backend/src/rotas.js
@@ -1,7 +1,10 @@
 const express=require('express');
 const DevController=require('./controller/devController');
+const LikeController=require('./controller/likeController');
 const router=express.Router(); //Cria um objeto que servirá para fazer as rotas.
 
 router.post("/devs",DevController.armazenar);   // Método que criará algo dentro da API REST.
+router.post("/devs/:IdDev/likes",LikeController.armazenar);
+// Esse ":algo" permite criar um parâmetro, que posteriormente pode ser recuperado dentro do método.
 
 module.exports=router;
