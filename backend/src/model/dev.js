8ed3258f9e3e1647a544b1d5a00ce1c7a79c509d
Rafael Teixeira <fael.teixeira@gmail.com>
2019-08-07 04:01:30 -0300

    Add comentários no model de dev.

--- backend/src/model/dev.js
+++ backend/src/model/dev.js
@@ -1,9 +1,9 @@
 const {Schema, model}=require("mongoose");
 
-const DevSchema=new Schema({
+const DevSchema=new Schema({    // Está definindo a estrutura da tabela Dev do banco de dados.
     nome:{
-        type: String,
-        required: true,
+        type: String,   // Isso diz que o campo é do tipo String.
+        required: true, // Isso diz que o campo é obrigatório no banco de dados.
     },
     usuario:{
         type:String,
@@ -15,7 +15,7 @@ const DevSchema=new Schema({
         required:true,
     }
 },{
-    timestamps:true,
+    timestamps:true,    // Isso cria automaticamente 2 campos - createdAt e updatedAt.
 });
 
 module.exports=model('Dev',DevSchema);
