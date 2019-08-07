97a3146b1bb6b7aa0db1e5f0f6486d60173ab9b8
Rafael Teixeira <fael.teixeira@gmail.com>
2019-08-07 17:16:37 -0300

    Definição dos likes e deslikes no model do dev.

--- backend/src/model/dev.js
+++ backend/src/model/dev.js
@@ -13,7 +13,17 @@ const DevSchema=new Schema({    // Está definindo a estrutura da tabela Dev do 
     avatar:{
         type:String,
         required:true,
-    }
+    },
+    likes: [{      // Ao abrir com colchete, signfica que haverá uma coleção de elementos, e não apenas um.
+        type: Schema.Types.ObjectId,    // Conterá os id's dos desenvolvedores que ele deu like.
+        ref:'Dev',                    // Diz a referência de qual model terá o id, no caso, do desenvolvedor.
+        // Isto "simula" um relacionamento em BDs relacionais.
+    }],
+    dislikes: [{      // Ao abrir com colchete, signfica que haverá uma coleção de elementos, e não apenas um.
+        type: Schema.Types.ObjectId,    // Conterá os id's dos desenvolvedores que ele deu like.
+        ref:'Dev',                    // Diz a referência de qual model terá o id, no caso, do desenvolvedor.
+        // Isto "simula" um relacionamento em BDs relacionais.
+    }],
 },{
     timestamps:true,    // Isso cria automaticamente 2 campos - createdAt e updatedAt.
 });
