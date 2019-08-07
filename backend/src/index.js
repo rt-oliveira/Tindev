224339a0f1bb074db49330ee0151eca42b67f3b1
Rafael Teixeira <fael.teixeira@gmail.com>
2019-08-07 02:38:40 -0300

    Olá mundo no Express, c/ retorno em JSON e params.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,11 @@
 const express=require('express');   // "Importou" o módulo express
 const servidor=express();           // Está criando um servidor
 
-servidor.get("/",function(req,res){             // Método que cria uma resposta para uma determinada
-    return res.send(`Olá ${req.query.name}`);   // rota (neste caso a rota raiz, só o endereço).
+servidor.get("/",function(req,res){                         // Método que cria uma resposta para uma 
+    return res.json({mensagem: `Olá ${req.query.name}`});   // determinada rota (a rota raiz, no caso).
+    // res.json faz com que um objeto seja retornado como resposta, em formato JSON.
     // req.query tem os parâmetros passados na URL, na requisição.
     // Exemplo: req.query.name
+
 });                                 
 servidor.listen(9999);              // Passará a 'escutar' requisições nesta porta
