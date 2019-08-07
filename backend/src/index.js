356398099efb89ab7cf89335f4ec242da804ee3a
Rafael Teixeira <fael.teixeira@gmail.com>
2019-08-07 02:26:49 -0300

    Olá mundo no Express/Node, c/ parâmetros na URL.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,9 @@
 const express=require('express');   // "Importou" o módulo express
 const servidor=express();           // Está criando um servidor
 
-servidor.get("/",function(req,res){ // Método que cria uma resposta para uma determinada
-    return res.send("Olá mundo");   // rota (neste caso a rota raiz, só o endereço).
+servidor.get("/",function(req,res){             // Método que cria uma resposta para uma determinada
+    return res.send(`Olá ${req.query.name}`);   // rota (neste caso a rota raiz, só o endereço).
+    // req.query tem os parâmetros passados na URL, na requisição.
+    // Exemplo: req.query.name
 });                                 
 servidor.listen(9999);              // Passará a 'escutar' requisições nesta porta
